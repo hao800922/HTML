e14d7ce3187692801cabd87b77f0689566f60d14
Hao <hao800922@gmail.com>
2020-07-08 12:44:20 +0800

    db+

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ const db = require(__dirname + '/db_connect');
 
 //=====================================================================================================
 
-app.get('/temp',(req, res)=>{
+app.get('/temp', (req, res) => {
     res.render('temp')
 })
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.locals.pageName = 'home';
     res.render('main', {
         name: 'project1',
     });
 });
 
-app.get('/course', (req, res)=>{
+app.get('/course', (req, res) => {
     res.render('course');
 });
 
@@ -28,31 +28,39 @@ app.get('/course', (req, res)=>{
 //     // res.json(req.body);
 // });
 
-app.get('/products', (req, res)=>{
+app.get('/products', (req, res) => {
     res.render('products');
 });
 
-app.get('/restaurant',(req, res)=>{
+app.get('/restaurant', (req, res) => {
     res.render('restaurant')
 })
 
-app.get('/login',(req, res)=>{
+app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.get('/register',(req, res)=>{
+app.get('/register', (req, res) => {
     res.render('register')
 })
+
+// 只是測試db有沒有連上線
+app.get('/try-db', (req, res) => {
+    const sql = "SELECT * FROM account";
+    db.query(sql)
+        .then(([r]) => {
+            res.json(r);
+        });
+});
 //=================================================================================================
 
 app.use(express.static('public'));
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.status(404).send('<h2>找不到頁面</h2>')
 });
 
-app.listen(3123, ()=>{
+app.listen(3123, () => {
     console.log('server started!');
 });
 
-// test123
