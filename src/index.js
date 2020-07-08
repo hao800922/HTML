c017fefca997883f93ae6f6ed14b83120b0f5e71
Hao <hao800922@gmail.com>
2020-07-08 13:24:03 +0800

    index+

--- src/index.js
+++ src/index.js
@@ -1,10 +1,88 @@
+// 呼叫套件或外掛js========================================================
 const express = require('express');
-
 const app = express();
+// express起手式
+// 套件express主要功用:
+// 1. 簡化並取代內件套件http及其相關方法
+// 2. 引入路由在適當時機取代相對路徑
+// 在專案下安裝 >npm i express
+// ref https://www.npmjs.com/package/express
 
-app.set('view engine', 'ejs');
+const { v4: uuidv4 } = require('uuid');
+// 引入uuidv4以供使用
+// 套件uuid會使用到的功用:
+// uuid中的v4為創建一個超多位數亂碼, 可作為不重複的代碼
+// 在專案下安裝 >npm i uuid
+// ref https://www.npmjs.com/package/uuid
+
+const session = require('express-session');
+// 引入express-session
+// 套件express-session功能:
+// 1. 可以設定用戶端的閒置時間
+// 2. 得知用戶端的session ID、查看用戶端使用資訊, e.g.登入次數、使用時間...
+// 在專案下安裝 >npm i express-session
+// ref https://www.npmjs.com/package/express-session
+
+
+const MysqlStore = require('express-mysql-session')(session);
+// 引入express-mysql-session
+// 套件express-mysql-session功能:
+// 存在記憶體的session資料, 備份到mysql上面
+// 在專案下安裝 >npm i express-mysql-session
+
+const moment = require('moment-timezone')
+// 引入moment-timezone
+// 套件moment-timezone功能:
+// 可將時間套用各種格式
+// 在專案下安裝 >npm i moment-timezone
 
 const db = require(__dirname + '/db_connect');
+// 呼叫外掛方式的database
+
+
+const sessionStore = new MysqlStore({}, db)
+// 將session存放在db之中
+// 1. 記得要放在 const db 下面
+// 2. 之所以有{}, 是因為全部用預設值即可
+
+
+app.set('view engine', 'ejs');
+// 啟動ejs_記得還要在專案下建立views資料夾
+// views資料夾將成為主要存取位置,相對路徑的起始點
+// 套件ejs功用:
+// 1. 切割與連結一個完整html頁面, 使主架構相同的頁面, 變換內容更加容易
+// 2. 跳脫以往格式的插入
+// 3. views資料夾將成為主要存取位置,相對路徑的起始點
+// 在專案下安裝 >npm i ejs
+// vs code 記得安裝模組ESJ language support, 不然會出現語法錯誤提示
+// ref https://www.npmjs.com/package/ejs
+
+// Top-level meddleware----------------------------------------------------
+app.use(express.urlencoded({ extended: false }));
+// 啟動urlencoded()
+// 用於form, 當用戶端以post的方式回傳queryString時, 轉匯成物件格式
+
+app.use(express.json());
+// 啟動json()
+// 將讀取資料轉換成 json
+
+// custom middleware----------------------------------------------------------------
+// 可以預設一些全域的變數，如果用戶修改將被取代
+app.use((req, res, next)=>{
+    res.locals.pageName=''; // 用來active按鈕
+    res.locals.email='null';
+    res.locals.password='0000';
+
+    // req.session.adminUser={
+    //     account:'hao',
+    //     nickname:'晧'
+    // };
+    res.locals.sess=req.session;
+
+ next()
+})
+//----------------------------------------------------------------------------------
+
 
 //=====================================================================================================
 
